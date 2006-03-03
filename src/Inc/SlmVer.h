281f6a04606a72a6e664a6af16e4d1fb715c40f0
Dogbones <Dogbones@svn-alleg.net>
2006-03-03 05:08:45 +0000

    version 022606 used for FAZ gold release
    git-svn-id: file:///media/data/aa/dev/Allegiance-SVN/branch/FAZDev@41 8c327b6c-a73e-8745-8738-111f1e176ae1

--- src/Inc/SlmVer.h
+++ src/Inc/SlmVer.h
@@ -1,6 +1,6 @@
 // mmf modified version, these set FileVersion and ProductVersion used by VerRes.rc
 #define rmj		1
 #define rmm		1
-#define rup		050652
+#define rup		022606
 #define szVerName	""
 #define szVerUser	"CURTC3"
