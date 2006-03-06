e91fcd7201f609a383dfcaced6229e039e3409fe
Tigereye <Tigereye@svn-alleg.net>
2006-03-06 04:12:31 +0000

    TE: Modified new AGCEvents to fire *before* being released (oops!) Modified all comments made by Tigereye to be consistent and say "TE". Some said TE, some said Tigereye. Now they all say "TE".
    git-svn-id: file:///media/data/aa/dev/Allegiance-SVN/branch/FAZDev@42 8c327b6c-a73e-8745-8738-111f1e176ae1

--- src/FedSrv/fsship.cpp
+++ src/FedSrv/fsship.cpp
@@ -595,13 +595,16 @@ void CFSShip::CaptureStation(IstationIGC * pstation)
   g.fm.SendMessages(GetMission()->GetGroupRealSides(), FM_GUARANTEED, FM_FLUSH);
 
   pstation->SetSide(pside);
-  // Tigereye, Fire AGCEvent when base is captured
+  // TE: Fire AGCEvent when base is captured
   CFSMission * pfsMission = this->GetMission();
   LPCSTR pszContext = pfsMission->GetIGCMission() ? pfsMission->GetIGCMission()->GetContextName() : NULL;
   _AGCModule.TriggerContextEvent(NULL, EventID_StationChangesSides, pszContext,
-    GetName(), GetShipID(), pside->GetUniqueID(), -1, 3,"OldTeam"    , VT_I4   , psideOld->GetUniqueID(),
-    "OldTeamName", VT_LPSTR, psideOld->GetName(),"StationName", VT_LPSTR, pstation->GetName());
-  //Tigereye end
+    GetName(), GetShipID(), pside->GetUniqueID(), -1, 4,
+	"GameID"	 , VT_I4   , pfsMission->GetMissionID(),
+	"OldTeam"    , VT_I4   , psideOld->GetUniqueID(),
+    "OldTeamName", VT_LPSTR, psideOld->GetName(),
+	"StationName", VT_LPSTR, pstation->GetName());
+  // TE end
   //Possibly the built themselves to a victory
   IsideIGC*   psideWin = m_pfsMission->CheckForVictoryByStationCapture(pside, psideOld);
   if (psideWin)
@@ -1034,7 +1037,7 @@ void CFSPlayer::SetSide(CFSMission * pfsMission, IsideIGC * pside)
     if (psideOld ) // if leaving a side
     {
       LPCSTR pszContext = GetIGCShip()->GetMission() ? GetIGCShip()->GetMission()->GetContextName() : NULL;
-	  // Tigereye Modify LeaveTeam AGCEvent to include MissionID, and change UniqueID to ObjectID
+	  // TE Modify LeaveTeam AGCEvent to include MissionID, and change UniqueID to ObjectID
       _AGCModule.TriggerContextEvent(NULL, EventID_LeaveTeam, pszContext,
         GetName(), idShip, psideOld->GetUniqueID(), -1, 3, // Changed UniqueID to ObjectID. Modified ParamCount to 3
         "MissionID", VT_I4	, psideOld->GetMission()->GetMissionID(),	// Added line MissionID as param
@@ -1050,7 +1053,7 @@ void CFSPlayer::SetSide(CFSMission * pfsMission, IsideIGC * pside)
 
     if (pside) // if joining a side
     {
-        // Tigereye Modify JoinTeam AGCEvent to include MissionID, and change UniqueID to ObjectID
+        // TE Modify JoinTeam AGCEvent to include MissionID, and change UniqueID to ObjectID
 		_AGCModule.TriggerEvent(NULL, EventID_JoinTeam, GetName(), idShip,
          pside->GetUniqueID(), -1, 3, // Changed UniqueID to ObjectID. Modified ParamCount to 3
 		 "MissionID", VT_I4	, pfsMission->GetMissionID(),		// Added line MissionID
