f42dedd37397b88d6386f7ebff8885061aa59f84
Tigereye <Tigereye@svn-alleg.net>
2006-03-23 15:09:32 +0000

    Reduced Launch Animation time from 5 seconds to 1 second (radar)
    git-svn-id: file:///media/data/aa/dev/Allegiance-SVN/branch/FAZDev@50 8c327b6c-a73e-8745-8738-111f1e176ae1

--- src/WinTrek/WinTrek.cpp
+++ src/WinTrek/WinTrek.cpp
@@ -4716,7 +4716,7 @@ public:
         m_cameraControl.SetOrientation (orthogonal);
 
         // this controls how long the launch animation lasts
-        m_timeOverrideStop = now + (bOverridePosition ? 5.0f : 3.0f);
+        m_timeOverrideStop = now + (bOverridePosition ? 1.0f : 0.75f);	// wlp: changed 5.0f:3.0f to 1.0f:0.75f
         
         m_bUseOverridePosition = bOverridePosition;
 
