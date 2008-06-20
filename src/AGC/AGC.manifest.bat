d8e7ee9c9001167818ba9ee98b47f5db2870d7d2
ImagoTrigger <Imago@svn-alleg.net>
2008-06-20 04:24:18 +0000

    AGC: manifest file for Makefile build no longer breaks project
    removed old appweb.lib
    
    imago@punkass.com
    
    git-svn-id: file:///media/data/aa/dev/Allegiance-SVN/branch/FAZR5Contrib@195 8c327b6c-a73e-8745-8738-111f1e176ae1

--- src/AGC/AGC.manifest.bat
+++ src/AGC/AGC.manifest.bat
@@ -1,2 +1,3 @@
 @echo off
-if %1 == Retail (copy AGC.retail.manifest AGC.manifest /y) else (copy AGC.debug.manifest AGC.manifest /y)
+if %1 == Retail (copy AGC.retail.manifest AGC.manifest /y) 
+if %1 == FZRetail (copy AGC.retail.manifest AGC.manifest /y) else (copy AGC.debug.manifest AGC.manifest /y)
