492f58b06b1388c5f0490af2e4f142dc33f26188
Dogbones <Dogbones@svn-alleg.net>
2008-07-28 13:11:48 +0000

    Test commit, part of doofus' dx9 update
    git-svn-id: file:///media/data/aa/dev/Allegiance-SVN/branch/FAZR5Contrib@212 8c327b6c-a73e-8745-8738-111f1e176ae1

--- src/Inc/SlmVer.h
+++ src/Inc/SlmVer.h
@@ -1,6 +1,6 @@
 // mmf modified version, these set FileVersion and ProductVersion used by VerRes.rc
 #define rmj		1
 #define rmm		1
-#define rup		100636  // mmf make sure this has 6 digits otherwise version number conversion for esc display may fail  - Imago says it will fail in the year 2064!
+#define rup		100226  // mmf make sure this has 6 digits otherwise version number conversion for esc display may fail
 #define szVerName	""
 #define szVerUser	"CURTC3"
