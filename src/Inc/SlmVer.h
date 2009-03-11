606076aee81794447db8bcc9a7f6a4220db205ef
Dogbones <Dogbones@svn-alleg.net>
2009-03-11 01:54:23 +0000

    Added rel to SlmVer.h for release number to the build string rjm.rmm.rel.rup
    git-svn-id: file:///media/data/aa/dev/Allegiance-SVN/branch/FAZR4@244 8c327b6c-a73e-8745-8738-111f1e176ae1

--- src/Inc/SlmVer.h
+++ src/Inc/SlmVer.h
@@ -1,6 +1,7 @@
 // mmf modified version, these set FileVersion and ProductVersion used by VerRes.rc
 #define rmj		1
 #define rmm		1
+#define rel     3       // mmf added to designate release number for a given build
 #define rup		100410  // mmf make sure this has 6 digits otherwise version number conversion for esc display may fail
 #define szVerName	""
 #define szVerUser	"CURTC3"
