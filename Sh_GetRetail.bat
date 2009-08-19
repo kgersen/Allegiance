7c6db85081509e1b9d2bef3e8182e8e9e9af04e4
ImagoTrigger <Imago@svn-alleg.net>
2009-08-19 05:54:50 +0000

    Added VC10 runtimes to GetRetail build helper script (Imago)
    git-svn-id: file:///media/data/aa/dev/Allegiance-SVN/branch/FAZR5Contrib@493 8c327b6c-a73e-8745-8738-111f1e176ae1

--- Sh_GetRetail.bat
+++ Sh_GetRetail.bat
@@ -8,3 +8,7 @@ copy objs10\FZRetail\WinTrek\Allegiance.exe x86
 copy objs10\FZRetail\WinTrek\Allegiance.PDB x86
 copy objs10\FZRetail\AGC\AGC.dll x86
 copy objs10\FZRetail\AGC\AGC.PDB x86
+copy "%VS100COMNTOOLS%..\..\VC\redist\x86\Microsoft.VC100.ATL\atl100.dll" x86
+copy "%VS100COMNTOOLS%..\..\VC\redist\x86\Microsoft.VC100.ATL\mfc100.dll" x86
+copy "%VS100COMNTOOLS%..\..\VC\redist\x86\Microsoft.VC100.CRT\msvcp100.dll" x86
+copy "%VS100COMNTOOLS%..\..\VC\redist\x86\Microsoft.VC100.CRT\msvcr100.dll" x86
