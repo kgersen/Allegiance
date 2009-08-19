d43e65a95a7f79f45b2b168021b3c6023ed0ea0f
ImagoTrigger <Imago@svn-alleg.net>
2009-08-19 05:45:29 +0000

    Added x86 release directory and script (Imago)
    git-svn-id: file:///media/data/aa/dev/Allegiance-SVN/branch/FAZR5Contrib@492 8c327b6c-a73e-8745-8738-111f1e176ae1

---
+++ Sh_GetRetail.bat
@@ -0,0 +1,10 @@
+copy objs10\FZRetail\FedSrv\AllSrv.exe x86
+copy objs10\FZRetail\FedSrv\AllSrv.PDB x86
+copy objs10\FZRetail\AllSrvUI\AllSrvUI.exe x86 
+copy objs10\FZRetail\AllSrvUI\AllSrvUI.PDB x86
+copy objs10\FZRetail\Lobby\AllLobby.exe x86
+copy objs10\FZRetail\Lobby\AllLobby.PDB x86
+copy objs10\FZRetail\WinTrek\Allegiance.exe x86 
+copy objs10\FZRetail\WinTrek\Allegiance.PDB x86
+copy objs10\FZRetail\AGC\AGC.dll x86
+copy objs10\FZRetail\AGC\AGC.PDB x86
