ac487d1d7445c13ac18f06330230ff1504c92e43
cort <cort@svn-alleg.net>
2009-11-18 22:28:05 +0000

    Bumped version number to "09.11.17".
    (That is the final version number of R5. The originally deployed build had version number "09.11.12".)
    
    git-svn-id: file:///media/data/aa/dev/Allegiance-SVN/branch/FAZR5Contrib@503 8c327b6c-a73e-8745-8738-111f1e176ae1

--- src/Inc/SlmVer.h
+++ src/Inc/SlmVer.h
@@ -2,6 +2,6 @@
 #define rmj		1
 #define rmm		5		// Imago (R#)
 #define rel     0       // mmf added to designate release number for a given build
-#define rup		111021  // mmf make sure this has 6 digits otherwise version number conversion for esc display may fail
+#define rup		111321  // mmf make sure this has 6 digits otherwise version number conversion for esc display may fail
 #define szVerName	""
 #define szVerUser	"CURTC3"
