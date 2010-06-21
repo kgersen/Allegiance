dde7a8c87edfccf52c8ebd50fc0868f25b09d043
ImagoTrigger <Imago@svn-alleg.net>
2010-06-21 10:31:15 +0000

    phase 2 of query server crap
    git-svn-id: file:///media/data/aa/dev/Allegiance-SVN/branch/FAZR6@564 8c327b6c-a73e-8745-8738-111f1e176ae1

--- src/FedSrv/qsrv.cpp
+++ src/FedSrv/qsrv.cpp
@@ -2,6 +2,52 @@
 
 //Imago 6/10 QueryServer
 
+#if 1 // #ifdef LITTLEENDIAN
+#if 0 //defined(_M_IX86)
+#define _ZEnd32( pData )     \
+    __asm {                 \
+            mov eax, *pData \
+            bswap eax       \
+            mov *pData, eax \
+          }
+
+#else
+#define _ZEnd32( pData )   \
+{                         \
+    char *c;              \
+    char temp;            \
+                          \
+    c = (char *) pData;   \
+    temp = c[0];          \
+    c[0] = c[3];          \
+    c[3] = temp;          \
+    temp = c[1];          \
+    c[1] = c[2];          \
+    c[2] = temp;          \
+}
+#endif
+
+#define _ZEnd16( pData )   \
+{                         \
+    char *c;              \
+    char temp;            \
+                          \
+    c = (char *) pData;   \
+    temp = c[0];          \
+    c[0] = c[1];          \
+    c[1] = temp;          \
+}
+
+#else  // not LITTLEENDIAN
+
+#define _ZEnd32(pData)
+#define _ZEnd16(pData)
+
+#endif // not LITTLEENDIAN
+
+#define ZEnd32(pData) _ZEnd32(pData)
+#define ZEnd16(pData) _ZEnd16(pData)
+
 const int kBufferSize = 1024;
         
 SOCKET SetUpListener(const char* pcAddress, int nPort);
@@ -79,79 +125,29 @@ DWORD WINAPI QueryHandler(void* sd_)
 
  bool UNSPackets(SOCKET sd)
 {
-	ZString zQRequest;
-	zQRequest.SetEmpty();
-	char acReadBuffer[kBufferSize] = {'\0'};
+
+	char acSendBuffer[kBufferSize] = {'\0'};
     int nReadBytes;
 
-	// qstat's bfs (BFRIS) is 0 based and wants us to start sending data immediately:
-	//	10 to 18 bytes				server info
-	//		10 bit						latency
-	//		11 bit						messaging ver
-	//		12 bit						max players
-	//		13 bit						1 &15 2 &16
-	//			1High?						game type
-	//			2Low?						autobalance
-	//	19 to a \0 NULL = n bytes		map name
-	//	n+1 to a \0 NULL = n bytes	player info
-	//		every 11 bytes				playername
-	//			+1 bit					ship
-	//			...						ping
-	//									kills
-	//									team
-	//									sector
-	//			6 bit					unused
-	//			7 to 10 bytes			little-endian integer score
+	// $> qstat.exe -P -R -bfs 192.168.0.252:25000 192.168.0.252:25001 ...
+	//TODO hook up real data!
+	memcpy(acSendBuffer,"AllegQstatN cz    MapName",25);
+	memcpy(acSendBuffer+26,"1zczcz     iMago",16);
+	memcpy(acSendBuffer+26+17,"2zczcz     ImAgo",16);
+	memcpy(acSendBuffer+26+17+17,"3zczcz     ImaGo",16);
 
-	ZString qResponse = "AllegQstatN cz    MapName\0Imago      czczcz1111Imago2     czczcz1111\0";
-	printf("Sending: %s\n",(PCC)qResponse);
-	int nTemp = send(sd, (PCC)qResponse,qResponse.GetLength(), 0); //ping 78, ver 32, players 99,   ?
-
+	int nTemp = send(sd,acSendBuffer,kBufferSize, 0);
 	if (nTemp > 0) {
-		printf("Sent %i bytes back to client.\n",2);
+		printf("Sent: %s to qstat\n",2);
 	}
 	else if (nTemp == SOCKET_ERROR) {
 		return false;
 	}
 	else {
-		printf("Peer unexpectedly dropped connection!\n");
+		printf("qstat unexpectedly dropped connection!\n");
 		return true;
 	}
-	//
-	/*
-    do {
-        nReadBytes = recv(sd, acReadBuffer, kBufferSize, 0);
-        if (nReadBytes > 0) {
-			zQRequest += acReadBuffer;
-		} else if (nReadBytes == SOCKET_ERROR) {
-            return false;
-        }
-    } while (nReadBytes != 0);
 
-	*/
-	//ZString zQResponse = GetQResponse(zQRequest); TODO
-
-    printf("Connection closed by peer.\nRequest: %s",(PCC)zQRequest);
     return true;
 }
-
- /* TODO
-   int nSentBytes = 0;
-            while (nSentBytes < nReadBytes) {
-                int nTemp = send(sd, acReadBuffer + nSentBytes,
-                        nReadBytes - nSentBytes, 0);
-                if (nTemp > 0) {
-                   printf("Sent %i bytes back to client.\n",nTemp);
-                    nSentBytes += nTemp;
-                }
-                else if (nTemp == SOCKET_ERROR) {
-                    return false;
-                }
-                else {
-                    printf("Peer unexpectedly dropped connection!\n");
-                    return true;
-                }
-            }
-        }
-		*/
 
