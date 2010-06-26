8fd66dd2ef9f8e881d8e544d64011af3a15d22d7
ImagoTrigger <Imago@svn-alleg.net>
2010-06-26 17:58:56 +0000

    Need 1.1's of these in our deployment!
    git-svn-id: file:///media/data/aa/dev/Allegiance-SVN/branch/FAZR6@575 8c327b6c-a73e-8745-8738-111f1e176ae1

--- Sh_GetRetail.bat
+++ Sh_GetRetail.bat
@@ -8,6 +8,10 @@ copy %2objs10\FZRetail\WinTrek\Allegiance.exe %1 /Y
 copy %2objs10\FZRetail\WinTrek\Allegiance.PDB %1 /Y
 copy %2objs10\FZRetail\AGC\AGC.dll %1 /Y
 copy %2objs10\FZRetail\AGC\AGC.PDB %1 /Y
+copy %2objs10\FZRetail\AutoUpdate\AutoUpdate.exe %1 /Y
+copy %2objs10\FZRetail\AutoUpdate\AutoUpdate.pdb %1 /Y
+copy %2objs10\FZRetail\Reloader\Reloader.exe %1 /Y
+copy %2objs10\FZRetail\Reloader\Reloader.pdb %1 /Y
 copy "%VS100COMNTOOLS%..\..\VC\redist\x86\Microsoft.VC100.ATL\atl100.dll" %1 /Y
 copy "%VS100COMNTOOLS%..\..\VC\redist\x86\Microsoft.VC100.MFC\mfc100.dll" %1 /Y
 copy "%VS100COMNTOOLS%..\..\VC\redist\x86\Microsoft.VC100.CRT\msvcp100.dll" %1 /Y
