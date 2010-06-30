2209b2e47a346988cf7d794f8f91da28e1f8897f
ImagoTrigger <Imago@svn-alleg.net>
2010-06-30 20:00:52 +0000

    pulled andons changes to asteroids - it changes too much (crashes server)  - less is more here, little change, test, little change, test --
    git-svn-id: file:///media/data/aa/dev/Allegiance-SVN/branch/FAZR6@605 8c327b6c-a73e-8745-8738-111f1e176ae1

--- src/Igc/asteroidigc.cpp
+++ src/Igc/asteroidigc.cpp
@@ -3,7 +3,7 @@
 **
 **  File:	asteroidIGC.cpp
 **
-**  Author:
+**  Author: 
 **
 **  Description:
 **      Implementation of the CasteroidIGC class. This file was initially created by
@@ -91,7 +91,7 @@ int         CasteroidIGC::Export(void* data) const
         dataAsteroid->signature = GetSignature();
 
         dataAsteroid->fraction = m_fraction;
-
+        
         const char* pszName = GetName();
         if (*pszName == '\0')
             memcpy(dataAsteroid->name, GetName(), sizeof(dataAsteroid->name));
@@ -113,34 +113,26 @@ struct AsteroidTypeRow
     AsteroidDef def;
 };
 
-static const AsteroidTypeRow asteroidTypes[] =  //andon, added more 6/10
+static const AsteroidTypeRow asteroidTypes[] = 
 {
-    { "asteroid",		"\0a",  { 0.0f, 0,        0,                      0, 25000, 400,  "bgrnd03", "", "meteoricon" } },
-    { "asteroid",		"\0a",  { 0.0f, 0,        0,                      0, 25000, 400,  "bgrnd05", "", "meteoricon" } },
-    { "asteroid",		"\0a",  { 0.0f, 0,        0,                      0, 25000, 400,  "bgrnd06", "", "meteoricon" } },
-    { "asteroid",		"\0a",  { 0.0f, 0,        0,                      0, 25000, 400,  "bgrnd07", "", "meteoricon" } },
-    { "asteroid",		"\0a",  { 0.0f, 0,        c_aabmBuildable,        0, 10000, 200,  "bgrnd03", "", "meteoricon" } },
-    { "asteroid",		"\0a",  { 0.0f, 0,        c_aabmBuildable,        0, 10000, 200,  "bgrnd05", "", "meteoricon" } },
-    { "asteroid",		"\0a",  { 0.0f, 0,        c_aabmBuildable,        0, 10000, 200,  "bgrnd06", "", "meteoricon" } },
-    { "asteroid",		"\0a",  { 0.0f, 0,        c_aabmBuildable,        0, 10000, 200,  "bgrnd07", "", "meteoricon" } },
-    { "Helium 3",		"He",   { 1.0f, 1.0f,     c_aabmMineHe3,          0, 25000, 100,  "bgrnd55", "", "heliumrock" } },
-    { "Helium 3",		"He",   { 1.0f, 1.0f,     c_aabmMineHe3,          0, 25000, 100,  "bgrnd56", "", "heliumrock" } },
-    { "Uranium",		"U",    { 0.0f, 0,        (c_aabmSpecial << 0),   0, 25000, 200,  "bgrnd51", "", "hotrock"    } },
-    { "Uranium",		"U",    { 0.0f, 0,        (c_aabmSpecial << 0),   0, 25000, 200,  "bgrnd59", "", "hotrock"    } },
-    { "Silicon",		"Si",   { 0.0f, 0,        (c_aabmSpecial << 1),   0, 25000, 200,  "bgrnd50", "", "copperrock" } },
-    { "Silicon",		"Si",   { 0.0f, 0,        (c_aabmSpecial << 1),   0, 25000, 200,  "bgrnd58", "", "copperrock" } },
-    { "Carbonaceous",	"C",    { 0.0f, 0,        (c_aabmSpecial << 2),   0, 25000, 200,  "bgrnd52", "", "carbonrock" } },
-    { "Carbonaceous",	"C",    { 0.0f, 0,        (c_aabmSpecial << 2),   0, 25000, 200,  "bgrnd53", "", "carbonrock" } },
+    { "asteroid",       "\0a",  { 0.0f, 0,        0,                      0, 25000, 400,  "bgrnd03", "", "meteoricon" } },
+    { "asteroid",       "\0a",  { 0.0f, 0,        0,                      0, 25000, 400,  "bgrnd05", "", "meteoricon" } },
+    { "asteroid",       "\0a",  { 0.0f, 0,        c_aabmBuildable,        0, 10000, 200,  "bgrnd03", "", "meteoricon" } },
+    { "asteroid",       "\0a",  { 0.0f, 0,        c_aabmBuildable,        0, 10000, 200,  "bgrnd05", "", "meteoricon" } },
+    { "Helium 3",       "He",   { 1.0f, 1.0f,     c_aabmMineHe3,          0, 25000, 100,  "bgrnd56", "", "heliumrock" } },
+    { "Uranium",        "U",    { 0.0f, 0,        (c_aabmSpecial << 0),   0, 25000, 200,  "bgrnd51", "", "hotrock"    } },
+    { "Silicon",        "Si",   { 0.0f, 0,        (c_aabmSpecial << 1),   0, 25000, 200,  "bgrnd52", "", "copperrock" } },
+    { "Carbonaceous",   "C",    { 0.0f, 0,        (c_aabmSpecial << 2),   0, 25000, 200,  "bgrnd53", "", "carbonrock" } }
 };
 
 const int nFirstHugeType = 0;
-const int nNumHugeTypes = 4; //andon increased, was 2 6/10
+const int nNumHugeTypes = 2;
 const int nFirstGenericType = nFirstHugeType + nNumHugeTypes;
-const int nNumGenericTypes = 4; //andon increased, was 2 6/10
+const int nNumGenericTypes = 2;
 const int nFirstMinableType = nFirstGenericType + nNumGenericTypes;
-const int nNumMinableTypes = 2;
+const int nNumMinableTypes = 1;
 const int nFirstSpecialType = nFirstMinableType + nNumMinableTypes;
-const int nNumSpecialTypes = 6; //andon increased, was 3 6/10 - shouldn't we make this be 6? --Imago CHANGED to 6 6/10
+const int nNumSpecialTypes = 3;
 const int numAsteroidTypes = sizeof(asteroidTypes) / sizeof(AsteroidTypeRow);
 
 static const AsteroidTypeRow& FindAsteroidRow(AsteroidAbilityBitMask aabm)
@@ -192,7 +184,7 @@ int IasteroidIGC::GetSpecialAsterioid(const MissionParams*  pmp, int index)
     {
         case 0:
         case 7:
-            n = index % 6; //andon, was 3 6/10
+            n = index % 3;
         break;
 
         case 1:
@@ -200,27 +192,27 @@ int IasteroidIGC::GetSpecialAsterioid(const MissionParams*  pmp, int index)
         break;
 
         case 2:
-            n = 2;
+            n = 1;
         break;
 
         case 3:
-            n = index % 4; //andon, was 2 6/10
+            n = index % 2;
         break;
 
         case 4:
-            n = 4;
+            n = 2;
         break;
 
         case 5:
-            n = ((index % 4) == 0) ? 0 : 4; //andon, was 2s 6/10
+            n = ((index % 2) == 0) ? 0 : 2;
         break;
 
         case 6:
-            n = 1 + (index % 4); //andon, 4 was 2 6/10
+            n = 1 + (index % 2);
         break;
     }
 
-    return n + nFirstSpecialType + randomInt(0, 1); //andon, added + randomInt(0, 1) 6/10
+    return n + nFirstSpecialType;
 }
 
 int IasteroidIGC::GetRandomType(AsteroidAbilityBitMask aabm)
@@ -230,15 +222,15 @@ int IasteroidIGC::GetRandomType(AsteroidAbilityBitMask aabm)
     switch (aabm)
     {
         case 0:
-            index = nFirstHugeType + randomInt(0, 3); //andon, increased from 1 to 3 6/10
+            index = nFirstHugeType + randomInt(0, 1);
         break;
 
         case c_aabmBuildable:
-            index = nFirstGenericType + randomInt(0, 3); //andon, increased from 1 to 3 6/10
+            index = nFirstGenericType + randomInt(0, 1);
         break;
 
         case c_aabmMineHe3:
-            index = nFirstMinableType + randomInt(0, 1); //andon, added + randomInt(0, 1) 6/10
+            index = nFirstMinableType;
         break;
 
         default:
