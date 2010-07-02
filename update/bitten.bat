5d76ad0a48e934878863af1e580bc06a04e08f3c
ImagoTrigger <Imago@svn-alleg.net>
2010-07-02 03:02:24 +0000

    Start running update tools from the checkout.  this will ease the pain of moving the build-slave in case of emergency or the unlikely event it is down.  This will be the first of many changes needed to get it to work "realativized"
    git-svn-id: file:///media/data/aa/dev/Allegiance-SVN/branch/FAZR6@610 8c327b6c-a73e-8745-8738-111f1e176ae1

--- update/bitten.bat
+++ update/bitten.bat
@@ -2,5 +2,5 @@ rem #Imago <imagotrigger@gmail.com> - Runs the bitten slave on a host
 
 :wut
 call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" x86
-call C:\Python26\Scripts\bitten-slave --log=bitten.log http://alleg.scarybugs.net/builds
+call C:\Python26\Scripts\bitten-slave -f bitten.ini -i 120 --log=bitten.log http://trac.alleg.net/builds
 goto wut
