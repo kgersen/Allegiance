8464c3d29bdb3bd189ce3bb450ff629e9aefd0c6
ImagoTrigger <Imago@svn-alleg.net>
2010-07-04 01:29:27 +0000

    simplified upgrade for CDN and made use of AutoUpdate shutdown - BETA next...
    git-svn-id: file:///media/data/aa/dev/Allegiance-SVN/branch/FAZR6@628 8c327b6c-a73e-8745-8738-111f1e176ae1

--- update/upgrade.pl
+++ update/upgrade.pl
@@ -1,86 +1,24 @@
 #Imago <imagotrigger@gmail.com>
-# Stops lobby/server, replaces objects, Starts lobby/server
+# Stops, Replaces, Reregisters and Starts AllSrv
 #  This file is for host CDN
 
 use strict;
-use Win32::OLE;
-use Net::Domain qw (hostname);
 use Win32::Process;
 
-my $bSstopped = 0;
-my $bLstopped = 0;
-
-my $s = Win32::OLE->GetObject("WinNT://".hostname()."/AllSrv,service");
-if ($s && $s->Status() == 4) {
-	#TODO block for up to an hour untill no running games
-	print "Stopping AllSrv service\n";	
-	$s->Stop();
-	$bSstopped = 1;
-	sleep(5);
-}
-
-my $sl = Win32::OLE->GetObject("WinNT://".hostname()."/AllLobby,service");
-if ($sl && $sl->Status() == 4 && (!$s || $s->Status() != 4)) {
-	print "Stopping AllLobby service\n";	
-	$sl->Stop();
-	$bLstopped = 1;
-	sleep(3);
-}
-
-if (($s && $s->Status == 4) || ($sl && $sl->Status == 4)) {
-	print "Services wouldn't shut down!\n";
-	exit 1;	
-}
-
-
-my $bSkilled = 0;
-my $bLkilled = 0;
-
-open (PS,"C:\\pslist.exe 2>crap AllSrv |");
-my $pid = 0;
-while (<PS>) {
-	if ($_ =~ /AllSrv\s+(\d+)/) {
-		$pid = $1;
-	}
-}
-close PS;
-
-if ($pid != 0) {
-	#TODO block for up to an hour untill no running games
-	my $cmd = "C:\\pskill 2>crap $pid";
-	print "Killing AllSrv executable $pid\n";
-	`$cmd`;
-	$bSkilled = 1;
-	sleep(5);
-	my $cmd = "regsvr32 C:\\AllegBeta\\AGC.dll /u /s";
-	`$cmd`;
-}
-
-open (PS,"C:\\pslist.exe 2>crap AllLobby |");
-my $pid = 0;
-while (<PS>) {
-	if ($_ =~ /AllLobby\s+(\d+)/) {
-		$pid = $1;
-	}
-}
-close PS;
-
-if ($pid != 0) {
-	#TODO block for up to an hour untill no running games
-	my $cmd = "C:\\pskill 2>crap $pid";
-	print "Killing AllLobby executable $pid\n";
-	`$cmd`;
-	$bLkilled = 1;
-	sleep(3);
-}
-
-
-	my $cmd = "regsvr32 C:\\AllegBeta\\AGC.dll /u /s";
-	`$cmd`;
+my $cmd = "C:\\AllegBeta\\AutoUpdate.exe";
+my $ProcessObj = "";
+Win32::Process::Create($ProcessObj,
+	$cmd,
+	"AutoUpdate shutdown",
+	0,
+	NORMAL_PRIORITY_CLASS,
+	"C:\\AllegBeta") || die "failed to create autoupdate.exe process\n";
 	
-	my $cmd = "regsvr32 C:\\AllegBeta\\AGC.dll /s";
-	`$cmd`;	
+sleep(1);
 
+my $cmd = "regsvr32 C:\\AllegBeta\\AGC.dll /u /s";
+`$cmd`;
+	
 my $cmd = "copy C:\\build\\FAZR6\\objs10\\FZDebug\\AGC\\AGC.dll C:\\AllegBeta\\AGC.dll /Y";
 `$cmd`;
 my $cmd = "copy C:\\build\\FAZR6\\objs10\\FZDebug\\FedSrv\\AllSrv.exe C:\\AllegBeta\\AllSrv.exe /Y";
@@ -94,48 +32,32 @@ my $cmd = "copy C:\\build\\FAZR6\\objs10\\FZDebug\\FedSrv\\AllSrv.pdb C:\\AllegB
 my $cmd = "copy C:\\build\\FAZR6\\objs10\\FZDebug\\Lobby\\AllLobby.pdb C:\\AllegBeta\\AllLobby.pdb /Y";
 `$cmd`;
 
+my $cmd = "regsvr32 C:\\AllegBeta\\AGC.dll /s";
+`$cmd`;	
 
-if ($bLstopped && $sl) {
-	print "Starting Lobby service\n";	
-	$sl->Start();
-	sleep(3);
-}
+sleep(1);
 
+my $cmd = "C:\\AllegBeta\\AllSrv.exe";
+print "Starting AllSrv executable\n";
+my $ProcessObj = "";
+Win32::Process::Create($ProcessObj,
+	$cmd,
+	"AllSrv -reregister",
+	0,
+	NORMAL_PRIORITY_CLASS|CREATE_NEW_CONSOLE,
+	"C:\\AllegBeta") || die "failed to create allsrv.exe reregister process\n";	
 
-if ($bSstopped && $s) {
+sleep(1);
 
-	print "Starting AllSrv service\n";	
-	$s->Start();
-	sleep(5);
-}
-
-if ($bLkilled) {
-	my $cmd = "C:\\AllegBeta\\AllLobby.exe";
-	print "Starting AllLobby executable\n";	
-	my $ProcessObj = "";
-	Win32::Process::Create($ProcessObj,
-					$cmd,
-					"AllLobby",
-					0,
-					NORMAL_PRIORITY_CLASS|CREATE_NEW_CONSOLE,
-					"C:\\AllegBeta")|| die ErrorReport();
-	sleep(3);
-}
-
-#if ($bSkilled) {
-	my $cmd = "C:\\AllegBeta\\AllSrv.exe";
-	print "Starting AllSrv executable\n";
-	my $ProcessObj = "";
-	Win32::Process::Create($ProcessObj,
-					$cmd,
-					"AllSrv",
-					0,
-					NORMAL_PRIORITY_CLASS|CREATE_NEW_CONSOLE,
-					"C:\\AllegBeta")|| die ErrorReport();
-
-	sleep(5);	
-#}
-
+my $cmd = "C:\\AllegBeta\\AllSrv.exe";
+my $ProcessObj = "";
+Win32::Process::Create($ProcessObj,
+	$cmd,
+	"AllSrv",
+	0,
+	NORMAL_PRIORITY_CLASS|CREATE_NEW_CONSOLE,
+	"C:\\AllegBeta") || die "failed to create allsrv.exe process\n";
+	
 
 exit 0;
 
