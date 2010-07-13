403f9ee30e236f0877343e7c504821d0f3123734
bard <bard@svn-alleg.net>
2010-07-13 16:49:58 +0000

    13 July 2010 Beta Wednesday. Changed R to 6.
    git-svn-id: file:///media/data/aa/dev/Allegiance-SVN/branch/FAZR6@701 8c327b6c-a73e-8745-8738-111f1e176ae1

--- src/Inc/SlmVer.h
+++ src/Inc/SlmVer.h
@@ -1,7 +1,7 @@
 // mmf modified version, these set FileVersion and ProductVersion used by VerRes.rc
 #define rmj		1
-#define rmm		5		// Imago (R#)
-#define rel     0       // mmf added to designate release number for a given build
-#define rup		111321  // mmf make sure this has 6 digits otherwise version number conversion for esc display may fail
+#define rmm		6		// Imago (R#)
+#define rel     0.03       // mmf added to designate release number for a given build
+#define rup		120715  // mmf make sure this has 6 digits otherwise version number conversion for esc display may fail
 #define szVerName	""
 #define szVerUser	"CURTC3"
