e0c4c2cb3d57317fb3856fa3f49b89b7b8642b58
ImagoTrigger <Imago@svn-alleg.net>
2010-07-15 18:22:11 +0000

    added dupe checking to file lists, prioritized which file to take if dupes, filelist now always contains whatever is on production AU (from au.pl).
    git-svn-id: file:///media/data/aa/dev/Allegiance-SVN/branch/FAZR6@724 8c327b6c-a73e-8745-8738-111f1e176ae1

--- update/makelist.pl
+++ update/makelist.pl
@@ -6,6 +6,8 @@ use strict;
 use POSIX qw(strftime);
 use File::Copy;
 
+my %dupes = ();
+
 print "Packaging deployment\n";
 
 open(LIST,">C:\\betalist.txt");
@@ -18,6 +20,10 @@ opendir(DIR, "C:\\build\\FAZR6\\Artwork\\");
 my @svn = readdir(DIR); 
 closedir DIR;
 
+opendir(DIR, "C:\\build\\Package\\tmp\\");
+my @tmp = readdir(DIR); 
+closedir DIR;
+
 my $offset = strftime("%z", localtime());
 if ($offset =~ /Daylight/i) {
 	$offset = 5;
@@ -25,15 +31,41 @@ if ($offset =~ /Daylight/i) {
 	$offset = 6;
 }
 
+
+#always include svn artwork on the list
+foreach my $file (@svn) { 
+	next if ($file =~ /^\./);
+	my $cmd = "C:\\crc32.exe C:\\build\\FAZR6\\Artwork\\$file";
+	my $cmd2 = "C:\\mscompress.exe C:\\build\\FAZR6\\Artwork\\$file";
+	my ($modtime,$size)= (stat("C:\\build\\FAZR6\\Artwork\\$file"))[9,7];
+	next if (!$size);
+	next if ($dupes{client}{$file} == 1);
+	$dupes{client}{$file} = 1;	
+	my $crc = `$cmd`;
+	chomp $crc;
+	$size = sprintf("%09d",$size);
+	my $dt = strftime("%Y/%m/%d %H:%M:%S",localtime($modtime + (3600 * $offset)));
+	my $crc2 = "0" x ( 8 - length($crc) ) . $crc; 
+	print LIST "$dt $size $crc2 $file\n";
+	if ($size < 2048 || $file =~ /\.avi|\.ogg|\.png/i) {
+		copy("C:\\build\\FAZR6\\Artwork\\${file}","C:\\build\\AutoUpdate\\Game\\$file");
+	} else {
+		`$cmd2`;
+		move("C:\\build\\FAZR6\\Artwork\\${file}_","C:\\build\\AutoUpdate\\Game\\$file");
+	}
+}
+
+
 #client list
 foreach my $file (@art) {
 	next if ($file =~ /^\./);
-	
 	next if ($file !~ /cc_09|tcor_|bgrnd/i); #demonstrates how to target specific client artwork using regexp filter
 	my $cmd = "C:\\crc32.exe C:\\build\\Package\\Artwork\\$file";
 	my $cmd2 = "C:\\mscompress.exe C:\\build\\Package\\Artwork\\$file";
 	my ($modtime,$size)= (stat("C:\\build\\Package\\Artwork\\$file"))[9,7];
 	next if (!$size);
+	next if ($dupes{client}{$file} == 1);
+	$dupes{client}{$file} = 1;	
 	my $crc = `$cmd`;
 	chomp $crc;
 	$size = sprintf("%09d",$size);
@@ -48,13 +80,15 @@ foreach my $file (@art) {
 	}
 }
 
-#always include svn artwork on the list
-foreach my $file (@svn) { 
+#include production AU artwork on the list!11!11 
+foreach my $file (@tmp) { 
 	next if ($file =~ /^\./);
-	my $cmd = "C:\\crc32.exe C:\\build\\FAZR6\\Artwork\\$file";
-	my $cmd2 = "C:\\mscompress.exe C:\\build\\FAZR6\\Artwork\\$file";
-	my ($modtime,$size)= (stat("C:\\build\\FAZR6\\Artwork\\$file"))[9,7];
+	my $cmd = "C:\\crc32.exe C:\\build\\Package\\tmp\\$file";
+	my $cmd2 = "C:\\mscompress.exe C:\\build\\Package\\tmp\\$file";
+	my ($modtime,$size)= (stat("C:\\build\\Package\\tmp\\$file"))[9,7];
 	next if (!$size);
+	next if ($dupes{client}{$file} == 1);
+	$dupes{client}{$file} = 1;	
 	my $crc = `$cmd`;
 	chomp $crc;
 	$size = sprintf("%09d",$size);
@@ -62,12 +96,13 @@ foreach my $file (@svn) {
 	my $crc2 = "0" x ( 8 - length($crc) ) . $crc; 
 	print LIST "$dt $size $crc2 $file\n";
 	if ($size < 2048 || $file =~ /\.avi|\.ogg|\.png/i) {
-		copy("C:\\build\\FAZR6\\Artwork\\${file}","C:\\build\\AutoUpdate\\Game\\$file");
+		copy("C:\\build\\Package\\tmp\\${file}","C:\\build\\AutoUpdate\\Game\\$file");
 	} else {
 		`$cmd2`;
-		move("C:\\build\\FAZR6\\Artwork\\${file}_","C:\\build\\AutoUpdate\\Game\\$file");
+		move("C:\\build\\Package\\tmp\\${file}_","C:\\build\\AutoUpdate\\Game\\$file");
 	}
 }
+
 
 #of course the latest build
 my @objs = ("C:\\Allegiance.exe","C:\\Reloader.exe");
@@ -97,7 +132,7 @@ unlink "C:\\build\\AutoUpdate\\Game.7z";
 my $cmd3 = "\"C:\\Program Files\\7-Zip\\7z.exe\" a -t7z C:\\build\\AutoUpdate\\Game.7z C:\\build\\AutoUpdate\\Game\\* -xr!*Server -mx9";
 system($cmd3);
 
-#now the server list, just the build..
+#now the server list, always the build..
 open(LIST,">C:\\serverlist.txt");
 my @objs = ("C:\\AllSrv.exe","C:\\AGC.dll", "C:\\AllSrvUI.exe","C:\\build\\External\\dbghelp.dll");
 foreach my $file (@objs) {
@@ -105,6 +140,8 @@ foreach my $file (@objs) {
 	my $cmd2 = "C:\\mscompress.exe $file";
 	my ($modtime,$size)= (stat("$file"))[9,7];
 	next if (!$size);
+	next if ($dupes{server}{$file} == 1);
+	$dupes{server}{$file} = 1;		
 	my $crc = `$cmd`;
 	chomp $crc;
 	$size = sprintf("%09d",$size);
@@ -119,13 +156,15 @@ foreach my $file (@objs) {
 	move("${file}_","C:\\build\\AutoUpdate\\Game\\Server\\$bin");
 }
 
-#lazy...always include svn artwork on the server list as well
+#always include svn artwork on the server list as well
 foreach my $file (@svn) { 
 	next if ($file =~ /^\./);
 	my $cmd = "C:\\crc32.exe C:\\build\\FAZR6\\Artwork\\$file";
 	my $cmd2 = "C:\\mscompress.exe C:\\build\\FAZR6\\Artwork\\$file";
 	my ($modtime,$size)= (stat("C:\\build\\FAZR6\\Artwork\\$file"))[9,7];
 	next if (!$size);
+	next if ($dupes{server}{$file} == 1);
+	$dupes{server}{$file} = 1;		
 	my $crc = `$cmd`;
 	chomp $crc;
 	$size = sprintf("%09d",$size);
@@ -137,6 +176,29 @@ foreach my $file (@svn) {
 	} else {
 		`$cmd2`;
 		move("C:\\build\\FAZR6\\Artwork\\${file}_","C:\\build\\AutoUpdate\\Game\\Server\\$file");
+	}
+}
+
+#lastly include whatever is on production AU
+foreach my $file (@svn) { 
+	next if ($file =~ /^\./);
+	my $cmd = "C:\\crc32.exe C:\\build\\Package\\tmp\\$file";
+	my $cmd2 = "C:\\mscompress.exe C:\\build\\Package\\tmp\\$file";
+	my ($modtime,$size)= (stat("C:\\build\\Package\\tmp\\$file"))[9,7];
+	next if (!$size);
+	next if ($dupes{server}{$file} == 1);
+	$dupes{server}{$file} = 1;	
+	my $crc = `$cmd`;
+	chomp $crc;
+	$size = sprintf("%09d",$size);
+	my $dt = strftime("%Y/%m/%d %H:%M:%S",localtime($modtime + (3600 * $offset)));
+	my $crc2 = "0" x ( 8 - length($crc) ) . $crc; 
+	print LIST "$dt $size $crc2 $file\n";
+	if ($size < 2048 || $file =~ /\.avi|\.ogg|\.png/i) {
+		copy("C:\\build\\Package\\tmp\\${file}","C:\\build\\AutoUpdate\\Game\\Server\\$file");
+	} else {
+		`$cmd2`;
+		move("C:\\build\\Package\\tmp\\${file}_","C:\\build\\AutoUpdate\\Game\\Server\\$file");
 	}
 }
 
