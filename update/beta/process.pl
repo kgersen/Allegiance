18d49f0f3d20e79ee268eb3dca64417c718bc86d
ImagoTrigger <Imago@svn-alleg.net>
2010-07-17 07:36:03 +0000

    more deployment order changes for beta - update au after servers are off but before lobby shuts down so it force updates the latest to any clients still connected
    git-svn-id: file:///media/data/aa/dev/Allegiance-SVN/branch/FAZR6@740 8c327b6c-a73e-8745-8738-111f1e176ae1

--- update/beta/process.pl
+++ update/beta/process.pl
@@ -22,27 +22,6 @@ system($cmd);
 my $cmd = "C:\\Perl\\bin\\perl.exe Z:\\deploy\\upgrade.pl";
 system($cmd);
 
-copy("Z:\\Deploy\\FAZBeta.cfg","Z:\\wwwroot\\FAZ\\FAZR6.cfg");
-copy("Z:\\Deploy\\FAZBeta.cfg","Z:\\wwwroot\\FAZ\\FAZBeta.cfg");
-copy("Z:\\Deploy\\FAZBeta.cfg","Z:\\wwwroot\\FAZ\\FAZR5.cfg");
-
-copy("Z:\\Deploy\\Filelist.txt","Z:\\wwwroot\\FAZ\\AU\\Filelist.txt");
-copy("Z:\\Deploy\\serverlist.txt","Z:\\wwwroot\\FAZ\\AU\\Standalone\\Filelist.txt");
-
-copy("Z:\\Deploy\\motdR6.mdl","Z:\\wwwroot\\FAZ\\motdR6.mdl");
-
-my $cmd = "Z:\\Deploy\\7za.exe x -y -oZ:\\wwwroot\\FAZ\\AU Z:\\deploy\\Game.7z";
-system($cmd);
-
-my $cmd = "Z:\\Deploy\\7za.exe x -y -oZ:\\wwwroot\\FAZ\\AU Z:\\deploy\\Server.7z";
-system($cmd);
-
-my $cmd = "Z:\\Deploy\\7za.exe x -y -oZ:\\wwwroot\\FAZ\\AU\\Standalone Z:\\deploy\\Server.7z";
-system($cmd);
-
-my $cmd = "expand Z:\\Deploy\\Filelist.txt C:\\Allegiance\\Lobby\\Filelist.txt";
-system($cmd);
-
 move("Z:\\deploy\\notify\\process", "Z:\\deploy\\notify\\ready");
 unlink 'process.pid';
 exit 0;
--- update/beta/upgrade.pl
+++ update/beta/upgrade.pl
@@ -37,6 +37,27 @@ if ($exitcode == 1) {
 	exit 1;
 }
 
+copy("Z:\\Deploy\\FAZBeta.cfg","Z:\\wwwroot\\FAZ\\FAZR6.cfg");
+copy("Z:\\Deploy\\FAZBeta.cfg","Z:\\wwwroot\\FAZ\\FAZBeta.cfg");
+copy("Z:\\Deploy\\FAZBeta.cfg","Z:\\wwwroot\\FAZ\\FAZR5.cfg");
+
+copy("Z:\\Deploy\\Filelist.txt","Z:\\wwwroot\\FAZ\\AU\\Filelist.txt");
+copy("Z:\\Deploy\\serverlist.txt","Z:\\wwwroot\\FAZ\\AU\\Standalone\\Filelist.txt");
+
+copy("Z:\\Deploy\\motdR6.mdl","Z:\\wwwroot\\FAZ\\motdR6.mdl");
+
+my $cmd = "Z:\\Deploy\\7za.exe x -y -oZ:\\wwwroot\\FAZ\\AU Z:\\deploy\\Game.7z";
+system($cmd);
+
+my $cmd = "Z:\\Deploy\\7za.exe x -y -oZ:\\wwwroot\\FAZ\\AU Z:\\deploy\\Server.7z";
+system($cmd);
+
+my $cmd = "Z:\\Deploy\\7za.exe x -y -oZ:\\wwwroot\\FAZ\\AU\\Standalone Z:\\deploy\\Server.7z";
+system($cmd);
+
+my $cmd = "expand Z:\\Deploy\\Filelist.txt C:\\Allegiance\\Lobby\\Filelist.txt";
+system($cmd);
+
 my $sl = Win32::OLE->GetObject("WinNT://beta.alleg.net/AllLobby,service");
 print "Stopping AllLobby service\n";	
 $sl->Stop();
