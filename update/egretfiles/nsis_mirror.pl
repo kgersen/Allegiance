674966b3fdb163b1ba64f3bd0cb381444c9a33c5
panzy <panzy@svn-alleg.net>
2010-07-18 08:16:41 +0000

    More simple Perl mistakes.  Thanks for the patience, Imago.
    git-svn-id: file:///media/data/aa/dev/Allegiance-SVN/branch/FAZR6@750 8c327b6c-a73e-8745-8738-111f1e176ae1

--- update/egretfiles/nsis_mirror.pl
+++ update/egretfiles/nsis_mirror.pl
@@ -4,8 +4,12 @@
 use strict;
 use Getopt::Long;
 use Net::FTP;
-use Digest::MD5;
 
+my $build = "";
+my $revision = "";
+my $version = "";
+my $ART = "C:\\inetpub\\wwwroot\\build\\AllegR6ART_b";
+my $PDB = "C:\\inetpub\\wwwroot\\build\\Alleg";
 my $server = "ftp.egretfiles.com";
 my $user = "build\@egretfiles.com";
 
@@ -13,16 +17,15 @@ open(PASS,"C:\\egretfiles.passwd");
 my $pass = <PASS>;
 close PASS;
 
-my $ART = "C:\\inetpub\\wwwroot\\build\\AllegR6ART_b";
-my $PDB = "C:\\inetpub\\wwwroot\\build\\AllegR6PDB_b";
+
 GetOptions("b=s" => \$build, "r=s" => \$revision, "v=s" => \$version);
-if($version == "1.1"){
+if($version eq "1.1"){
 	$ART .= "$build_r$revision.exe";
-	$PDB .= "$build_r$revision.exe";
+	$PDB .= "R6PDB_b$build_r$revision.exe";
 }
-else if($version == "1.0"){
+else if($version eq "1.0"){
 	$ART .= "$build_r$revision.exe";
-	$PDB .= "$build_r$revision.exe";
+	$PDB .= "PDB_b$build_r$revision.exe";
 }
 else{ die "No version specified! Command line switch '-v' either 1.1 or 1.0"; }
 
@@ -31,6 +34,7 @@ my $ftp = Net::FTP->new($server, Debug => 0, Port => 21, Passive => 1)
 
 $ftp->login($user,$pass)
 	or die "Cannot login ", $ftp->message;
+$ftp->binary;
 print "Uploading PDB to $server\n";
 $ftp->put($PDB)
 	or die "Put failed ", $ftp->message;    
