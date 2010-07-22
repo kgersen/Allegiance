6bda26df3adc358abdf8471215b9ccb561960e28
ImagoTrigger <Imago@svn-alleg.net>
2010-07-22 19:06:15 +0000

    put the actual installer on the CloudNAS, not just the stuff the installer downloads (components)
    git-svn-id: file:///media/data/aa/dev/Allegiance-SVN/branch/FAZR6@784 8c327b6c-a73e-8745-8738-111f1e176ae1

--- update/cloud/nsis_mirror.pl
+++ update/cloud/nsis_mirror.pl
@@ -11,6 +11,7 @@ my $revision = "";
 my $version = "";
 my $ART = "C:\\inetpub\\wwwroot\\build\\AllegR6ART_b";
 my $PDB = "C:\\inetpub\\wwwroot\\build\\Alleg";
+my $installer = "C:\\inetpub\\wwwroot\\build\\" #Imago 7/10
 my $server = "10.0.0.5";
 my $user = "cloudnas";
 
@@ -19,13 +20,16 @@ my $pass = <PASS>;
 close PASS;
 
 
+
 GetOptions("b=s" => \$build, "r=s" => \$revision, "v=s" => \$version);
 if($version eq "1.1"){
 	$ART .= "${build}_r${revision}.exe";
 	$PDB .= "R6PDB_b${build}_r${revision}.exe";
+	$installer = "R6_b${build}_r${revision}.exe"
 }
 elsif($version eq "1.0"){	# Fuzz 07/18 ART is already pushed in 1.1
 	$PDB .= "PDB_b${build}_r${revision}.exe";
+	$installer = "Alleg_b${build}_r${revision}.exe"
 }
 else{ die "No version specified! Command line switch '-v' either 1.1 or 1.0"; }
 
@@ -38,6 +42,9 @@ $ftp->binary;
 print "Uploading PDB to $server\n";
 $ftp->put($PDB)
 	or die "Put failed ", $ftp->message;
+print "Uploading Installer to $server\n";	
+$ftp->put($installer)
+	or die "Put failed ", $ftp->message;	
 if($version eq "1.1"){
 	print "Uploading ART to $server\n";
 	$ftp->put($ART)
