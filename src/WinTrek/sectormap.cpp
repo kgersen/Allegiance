a7d46bdff2c6591bbd06322cd745e76651078a78
ImagoTrigger <Imago@svn-alleg.net>
2010-08-09 02:41:57 +0000

    enh. to #129
    git-svn-id: file:///media/data/aa/dev/Allegiance-SVN/branch/FAZR6@926 8c327b6c-a73e-8745-8738-111f1e176ae1

--- src/WinTrek/sectormap.cpp
+++ src/WinTrek/sectormap.cpp
@@ -333,14 +333,18 @@ public:
 		for (ProbeLinkIGC*  ppl = pcluster->GetProbes()->first(); (ppl != NULL); ppl = ppl->next())
         {
             IprobeIGC*  pprobe = ppl->data();			
-            if (pprobe->GetSide() == trekClient.GetShip()->GetSide())
+            if (pprobe->GetSide() == trekClient.GetShip()->GetSide() && 
+				(pprobe->GetProbeType()->GetCapabilities() == 0 || pprobe->GetProbeType()->GetCapabilities() >= c_eabmRescue)) //Imago 8/10
 				probeCount++;
 		}
+		//Imago 8/10
+		Surface * picon = (Surface *)trekClient.LoadRadarIcon("probe");
+		pcontext->DrawImage3D(picon, trekClient.GetSide()->GetColor(), false, ptNext);
 		pcontext->DrawString(
                 pfont,
                 Color::White(),
-                ptNext,
-                "Probes: " + ZString(probeCount)
+                ptNext + Point(picon->GetSize().X() + 4, 0),
+                ZString(probeCount)
 				);
 
 
