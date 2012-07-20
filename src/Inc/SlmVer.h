fc1c143b89fd5675bd1924b9f41b0ed840a14cb2
pkk <pkk@svn-alleg.net>
2012-07-20 13:30:53 +0000

    Merged trunk changes with branches and fixed VS2008 (zlib project file)
    git-svn-id: file:///media/data/aa/dev/Allegiance-SVN/branch/main@1070 8c327b6c-a73e-8745-8738-111f1e176ae1

--- src/Inc/SlmVer.h
+++ src/Inc/SlmVer.h
@@ -2,7 +2,7 @@
 #define rmj		1
 #define rmm		6		// Imago (R#)
 #define rel     0.03       // mmf added to designate release number for a given build
-#define rup		140206  // mmf make sure this has 6 digits otherwise version number conversion for esc display may fail
+#define rup		140516  // mmf make sure this has 6 digits otherwise version number conversion for esc display may fail
 						// numbers are octal, YYMMDD
 #define szVerName	""
 #define szVerUser	"CURTC3"
