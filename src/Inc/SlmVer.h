c646c3b7dad5d83525a8e114c5e177067865696b
pkk <pkk@svn-alleg.net>
2013-08-18 17:14:11 +0000

    Updated version/build vars
    git-svn-id: file:///media/data/aa/dev/Allegiance-SVN/branch/main@1147 8c327b6c-a73e-8745-8738-111f1e176ae1

--- src/Inc/SlmVer.h
+++ src/Inc/SlmVer.h
@@ -1,9 +1,9 @@
 // mmf modified version, these set FileVersion and ProductVersion used by VerRes.rc
 #define rmj		1
-#define rmm		7		// Imago (R#)
+#define rmm		8		// Imago (R#)
 #define rel     0.00       // mmf added to designate release number for a given build
 
-#define rup		141235  // mmf make sure this has 6 digits otherwise version number conversion for esc display may fail,
+#define rup		151022  // mmf make sure this has 6 digits otherwise version number conversion for esc display may fail,
 						// rup is specified in octal, so for instance 11/9/2010 (US format)
 						// Year: 2010 - use last two digits: 10, convert to octal = 12.
 						// Month: 11 - convert to octal = 13
