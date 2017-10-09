b95c9448a288cd634d6e95ee8fb585edbb40d0bb
Nick Pirocanac <nick@zaphop.com>
2017-10-09 18:34:21 -0500

    Fixing MoGas crash on invalid model load.

--- src/Igc/modelIGC.cpp
+++ src/Igc/modelIGC.cpp
@@ -147,7 +147,13 @@ HRESULT     CmodelIGC::Load(int                options,
 {
     LoadCVH(model, icon, attributes);
 
-    HRESULT rc = m_pThingSite->LoadModel(options, model, texture);
+	HRESULT rc;
+	
+	// BT - 10/17 - Fixing MoGas crash on model load fail.
+	if (m_pThingSite == nullptr)
+		rc = E_FAIL;
+	else
+		rc = m_pThingSite->LoadModel(options, model, texture);
 
     if (SUCCEEDED(rc))
     {
