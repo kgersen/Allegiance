e4f9d668e4d142c449c226ac32a1d6b7b2d19581
Nick Pirocanac <nick@zaphop.com>
2017-10-10 23:43:44 -0500

    Replatforming to support WinXP. Note: VC Runtimes for VC2008sp1 are required to run on XP. These will be added to the steam installer as a pre-req.

--- src/WinTrek/WinTrek.h
+++ src/WinTrek/WinTrek.h
@@ -29,6 +29,11 @@ const float s_fMinFOV = RadiansFromDegrees(5.0f);
 class TrekWindowImpl;
 class AutoDownloadProgressDialogPopup; // MGC: move
 
+// BT - 10/17 - Work-around for backwards compatibility with delayimp.h and the Windows XP targeting pack for VS2017.
+#ifndef FACILITY_VISUALCPP
+	#define FACILITY_VISUALCPP  ((LONG)0x6d)
+#endif
+
 /*!!!
 class BillboardType {
 public:
