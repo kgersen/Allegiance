93529b5ab5114f75defef85a93d4af0772146212
Radulfr4 <33282887+Radulfr4@users.noreply.github.com>
2017-11-08 08:33:54 +0100

    undo last
    Where is the button for this?

--- src/training/Training.h
+++ src/training/Training.h
@@ -40,7 +40,6 @@ namespace Training
     void            SetSkipPostSlideshow (void);
     bool            IsTraining (void);
     bool            IsInstalled (void);
-    bool            CommandViewEnabled(void);
     int             GetTrainingMissionID (void);
     SectorID        GetStartSectorID (void);
     void            SetupShipAndCamera (void);
