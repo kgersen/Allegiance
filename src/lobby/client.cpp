0ea0a3e80a23438aacbf13929a8117afd869defb
Nick Pirocanac <nick@zaphop.com>
2017-11-11 12:25:51 -0600

    Reapplying LAN server compatibility for IPv6.

--- src/lobby/client.cpp
+++ src/lobby/client.cpp
@@ -80,7 +80,7 @@ static DWORD GetRegDWORD(const char* szKey, DWORD dwDefault)
 //	FedMessaging & fm = g_pLobbyApp->GetFMClients();
 //	CFMConnection * pcnxn = fm.GetConnectionFromId(pqd->dwConnectionID);
 //	
-//	char szAddress[16];
+//	char szAddress[64];
 //	Strcpy(szAddress, "");
 //
 //	fm.GetIPAddress(*pcnxn,szAddress);
@@ -411,7 +411,7 @@ HRESULT LobbyClientSite::OnAppMessage(FedMessaging * pthis, CFMConnection & cnxn
 		{
 			if (!pServerT->GetPaused()) // not paused
 			{
-				char szRemoteAddress[16];
+				char szRemoteAddress[64];
 				g_pLobbyApp->GetFMServers().GetIPAddress(*iterCnxn.Value(), szRemoteAddress);
 				//IMAGO REVIEW: Sanity check on szAddr first
 				if (strcmp(szRemoteAddress,szAddr)==0) // IPs match
@@ -479,7 +479,7 @@ HRESULT LobbyClientSite::OnAppMessage(FedMessaging * pthis, CFMConnection & cnxn
       {
         BEGIN_PFM_CREATE(*pthis, pfmJoinMission, L, JOIN_MISSION)
         END_PFM_CREATE
-        char szServer[16];
+        char szServer[64];
         g_pLobbyApp->GetFMServers().GetIPAddress(*pMission->GetServer()->GetConnection(), szServer);
         assert(lstrlen(szServer) < sizeof(pfmJoinMission->szServer)); // as long as szServer is fixed length
         Strcpy(pfmJoinMission->szServer, szServer);
