9917b81628c5397d010964d277b2bc5717e6ec72
Radulfr <crapma@gmx.net>
2017-11-15 18:14:00 +0100

    Making stations EXPLODE!

--- src/training/SetStationDestroyedAction.cpp
+++ src/training/SetStationDestroyedAction.cpp
@@ -22,7 +22,15 @@ namespace Training
     //------------------------------------------------------------------------------
     /* void */  SetStationDestroyedAction::SetStationDestroyedAction (ImodelIGC* theStation)
     {
-       pStation = theStation;
+       pStation = static_cast<IstationIGC*>(theStation);
+       ZAssert(pStation);
+    }
+
+    //------------------------------------------------------------------------------
+    /* void */  SetStationDestroyedAction::SetStationDestroyedAction(IstationIGC* theStation)
+    {
+        pStation = theStation;
+        ZAssert(pStation);
     }
 
     //------------------------------------------------------------------------------
@@ -33,7 +41,23 @@ namespace Training
     //------------------------------------------------------------------------------
     void        SetStationDestroyedAction::Execute (void)
     {
-		if (pStation)
-			pStation->Terminate();
+        if (pStation) {
+            pStation->GetCluster()->GetClusterSite()->AddExplosion(pStation,
+                pStation->GetStationType()->HasCapability(c_sabmFlag)
+                ? c_etLargeStation
+                : c_etSmallStation);
+
+            if (pStation->GetSide() == trekClient.GetSide())
+                trekClient.PlaySoundEffect(pStation->GetStationType()->GetDestroyedSound());
+            else
+                trekClient.PlaySoundEffect(pStation->GetStationType()->GetEnemyDestroyedSound());
+
+            trekClient.PostText(true, "The " START_COLOR_STRING "%s" END_COLOR_STRING " station has been destroyed.",
+                (PCC)ConvertColorToString(pStation ? pStation->GetSide()->GetColor() : Color::White()),
+                (pStation ? pStation->GetName() : "Unknown station")
+            );
+
+            pStation->Terminate();
+        }
     }
 }
