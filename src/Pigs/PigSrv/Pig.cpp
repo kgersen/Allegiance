a337980afdf993698aeda7f71663f03e120764f6
Austin Harris <austin.w.harris@gmail.com>
2017-12-19 23:23:17 -0700

    Revert "Don't call out to PigAccounts"
    This reverts commit 073f14189f24c6b06e7607a86487202161c6c85a.

--- src/Pigs/PigSrv/Pig.cpp
+++ src/Pigs/PigSrv/Pig.cpp
@@ -764,6 +764,14 @@ unsigned CALLBACK CPig::ThreadThunk(void* pvParam)
 
 HRESULT CPig::ThreadCreatePig(CPig::XThreadParams* ptp, CComObject<CPig>*& pPig)
 {
+  // Get the account dispenser
+  IPigAccountDispenserPtr spDispenser;
+  RETURN_FAILED(GetEngine().get_AccountDispenser(&spDispenser));
+
+  // Get an available account from the dispenser
+  IPigAccountPtr spAccount;
+  RETURN_FAILED(spDispenser->get_NextAvailable(&spAccount));
+
   // Lock static variables during the object creation
   ptp->m_hr = pPig->CreateInstance(&pPig);
   IPigPtr spPig(pPig);
@@ -772,7 +780,8 @@ HRESULT CPig::ThreadCreatePig(CPig::XThreadParams* ptp, CComObject<CPig>*& pPig)
   assert(NULL != spPig);
 
   // Set the pig's account and cache the account name
-  pPig->m_bstrName = "yoohoo";
+  pPig->m_spAccount = spAccount;
+  RETURN_FAILED(spAccount->get_Name(&pPig->m_bstrName));
 
   // Create an instance of a behavior object attached to the pig
   CComObject<CPigBehaviorScript>* pBehavior = NULL;
@@ -2118,6 +2127,7 @@ STDMETHODIMP CPig::get_Money(AGCMoney* pnMoney)
 STDMETHODIMP CPig::Logon()
 {
   _TRACE1("CPig::Logon(): GetCurrentThreadId = %u\n", GetCurrentThreadId());
+  assert(NULL != m_spAccount);
 
   // Validate the current state
   if (PigState_NonExistant != GetCurrentState())
@@ -2148,8 +2158,6 @@ STDMETHODIMP CPig::Logon()
 
   // Copy the pig account name and password to the connection parameters
   lstrcpy(ci.szName, OLE2CA(m_bstrName));
-  //ci.steamAuthTicket = 0;
-  //ci.steamAuthTicketLength = 0;
   
 #ifdef USEAUTH
   // Authenticate the account on the Zone authentication server, if any
