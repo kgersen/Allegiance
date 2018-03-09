8e4ea235b83f152ce04347af77bef15419aabf8e
Radulfr <crapma@gmx.net>
2018-03-09 16:07:39 +0100

    Tower priority based on AOE; Start firing earlier

--- src/Igc/probeigc.cpp
+++ src/Igc/probeigc.cpp
@@ -380,7 +380,7 @@ void    CprobeIGC::Update(Time now)
 
                 //If we have a target ... find the closest enemy ship who is a valid target
                 ExpendableAbilityBitMask    eabm = m_probeType->GetCapabilities();
-                float       distance2Min = speed * lifespan / 1.2f;
+                float       distance2Min = speed * lifespan / 1.05f;
                 distance2Min *= distance2Min;
                 Vector      directionMin;
 
@@ -406,6 +406,14 @@ void    CprobeIGC::Update(Time now)
                             &pmodelTarget, &distance2Min, &directionMin);
                     }
 
+                    bool hasAOE = (GetProjectileType()->GetBlastRadius() > 1.0f);
+                    if (hasAOE && !pmodelTarget && (eabm & c_eabmShootMissiles))
+                    {
+                        GetTarget((const ModelListIGC*)(pcluster->GetMissiles()),
+                            pside, myPosition, dtUpdate, accuracy, speed, lifespan, OT_missile,
+                            &pmodelTarget, &distance2Min, &directionMin);
+                    }
+
                     if (!pmodelTarget && (eabm & c_eabmShootShips))
                     {
                         GetTarget((const ModelListIGC*)(pcluster->GetShips()),
@@ -413,7 +421,7 @@ void    CprobeIGC::Update(Time now)
                                   &pmodelTarget, &distance2Min, &directionMin);
                     }
 
-                    if (!pmodelTarget && (eabm & c_eabmShootMissiles))
+                    if (!hasAOE && !pmodelTarget && (eabm & c_eabmShootMissiles))
                     {
                         GetTarget((const ModelListIGC*)(pcluster->GetMissiles()),
                                   pside, myPosition, dtUpdate, accuracy, speed, lifespan, OT_missile, 
