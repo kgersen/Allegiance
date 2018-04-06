c961885b97e419abed5be8c0a4d3d0d2d489d77e
Peter van Dommelen <pvandommelen@users.noreply.github.com>
2018-04-06 14:22:34 +0200

    Removed ServerAddress

--- src/clintlib/clintlib.cpp
+++ src/clintlib/clintlib.cpp
@@ -1285,27 +1285,7 @@ HRESULT BaseClient::ConnectToServer(ConnectInfo & ci, DWORD dwCookie, Time now, 
 			hr = m_fm.JoinSession(FEDSRV_STANDALONE_PRIVATE_GUID, ci.strServer, ci.szName);		  
 		}
 		else {
-			//imago moved this up first 8/1/09 due to longer timeout
-            HKEY hKey;
-            DWORD cbValue = c_cbName;
-            char szServer[c_cbName];
-            szServer[0] = '\0';
-            if (ERROR_SUCCESS == RegOpenKeyEx(HKEY_CURRENT_USER, ALLEGIANCE_REGISTRY_KEY_ROOT, 0, KEY_READ, &hKey)) {
-                ::RegQueryValueEx(hKey,"ServerAddress", NULL, NULL, (unsigned char*)&szServer, &cbValue);
-                ::RegCloseKey(hKey);
-            }
-            if (szServer[0] != '\0') {
-                ZString strServer = ZString(szServer).LeftOf(":");
-                DWORD dwPort = ZString(szServer).RightOf(":").GetInteger();
-
-                hr = m_fm.JoinSession(FEDSRV_GUID, strServer, ci.szName, dwPort);
-			} else {
-				hr = -1;
-			}
-
-			if(FAILED(hr)) 
-				hr = m_fm.JoinSession(FEDSRV_GUID, ci.strServer, ci.szName, ci.dwPort);
-
+            hr = m_fm.JoinSession(FEDSRV_GUID, ci.strServer, ci.szName, ci.dwPort);
         }
     }
 
