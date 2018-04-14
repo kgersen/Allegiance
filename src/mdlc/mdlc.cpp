abe855c66a6b97657480f6fb5971297c324e7c2e
Peter van Dommelen <pvandommelen@users.noreply.github.com>
2018-04-14 15:45:48 +0200

    Fixed compile of mdlc

--- src/mdlc/mdlc.cpp
+++ src/mdlc/mdlc.cpp
@@ -268,9 +268,16 @@ public:
 		//Imago set the modeler up to work in the CWD
 		PathString pathStr = pathStr.GetCurrentDirectoryA();
 		printf(pathStr);
+
+        TRef<UpdatingConfiguration> pConfiguration = new UpdatingConfiguration(
+            std::make_shared<FallbackConfigurationStore>(
+                CreateJsonConfigurationStore(GetExecutablePath() + "\\config.json"),
+                std::make_shared<RegistryConfigurationStore>(HKEY_CURRENT_USER, ALLEGIANCE_REGISTRY_KEY_ROOT "\\MDLC3DSettings")
+                )
+        );
         
 		// Imago DX9 junk
-		if( PromptUserForVideoSettings(false, 0, GetModuleHandle(NULL), pathStr, ALLEGIANCE_REGISTRY_KEY_ROOT "\\MDLC3DSettings") == false )
+		if( PromptUserForVideoSettings(false, 0, GetModuleHandle(NULL), pathStr, pConfiguration) == false )
 		{
 			return E_FAIL;
 		}
